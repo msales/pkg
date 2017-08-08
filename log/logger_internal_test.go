137516af7d2a66d9f07714689255592ec8de2e87
Nicholas Wiersma <nick@wiersma.co.za>
2017-08-08 15:03:57 +0200

    Add log tests

---
+++ log/logger_internal_test.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithLogger(t *testing.T) {
+	ctx := WithLogger(context.Background(), Null)
+
+	got := ctx.Value(ctxKey)
+
+	assert.Equal(t, Null, got)
+}
+
+func TestFromContext(t *testing.T) {
+	ctx := context.WithValue(context.Background(), ctxKey, Null)
+
+	got, ok := FromContext(ctx)
+
+	assert.True(t, ok)
+	assert.Equal(t, Null, got)
+}
+
+func TestFromContext_NotSet(t *testing.T) {
+	ctx := context.Background()
+
+	got, ok := FromContext(ctx)
+
+	assert.False(t, ok)
+	assert.Nil(t, got)
+}
+
+func TestWithLoggerFunc(t *testing.T) {
+	tests := []struct {
+		ctx    context.Context
+		expect Logger
+	}{
+		{context.Background(), Null},
+	}
+
+	for _, tt := range tests {
+		withLogger(tt.ctx, func(l Logger) {
+			assert.Equal(t, tt.expect, l)
+		})
+	}
+}
---
+++ log/logger_test.go
@@ -0,0 +1,58 @@
+package log_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/msales/pkg/log"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestDebug(t *testing.T) {
+	m := new(MockLogger)
+	m.On("Debug", "test log", []interface{}{"foo", "bar"})
+
+	ctx := log.WithLogger(context.Background(), m)
+
+	log.Debug(ctx, "test log", "foo", "bar")
+
+	m.AssertExpectations(t)
+}
+
+func TestInfo(t *testing.T) {
+	m := new(MockLogger)
+	m.On("Info", "test log", []interface{}{"foo", "bar"})
+
+	ctx := log.WithLogger(context.Background(), m)
+
+	log.Info(ctx, "test log", "foo", "bar")
+
+	m.AssertExpectations(t)
+}
+
+func TestError(t *testing.T) {
+	m := new(MockLogger)
+	m.On("Error", "test log", []interface{}{"foo", "bar"})
+
+	ctx := log.WithLogger(context.Background(), m)
+
+	log.Error(ctx, "test log", "foo", "bar")
+
+	m.AssertExpectations(t)
+}
+
+type MockLogger struct {
+	mock.Mock
+}
+
+func (m MockLogger) Debug(msg string, ctx ...interface{}) {
+	m.Called(msg, ctx)
+}
+
+func (m MockLogger) Info(msg string, ctx ...interface{}) {
+	m.Called(msg, ctx)
+}
+
+func (m MockLogger) Error(msg string, ctx ...interface{}) {
+	m.Called(msg, ctx)
+}
