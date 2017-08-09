3847e092d838653a3475849932b01462674d7edf
Nicholas Wiersma <nick@wiersma.co.za>
2017-08-09 08:32:24 +0200

    Stop runtime failing

--- stats/runtime_test.go
+++ stats/runtime_test.go
@@ -10,13 +10,12 @@ import (
 
 func TestRuntime(t *testing.T) {
 	m := new(MockStats)
-	m.On("Gauge", "runtime.cpu.goroutines", mock.Anything, mock.Anything, mock.Anything).Return(nil)
 	m.On("Gauge", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
-	stats.DefaultRuntimeInterval = 10 * time.Microsecond
+	stats.DefaultRuntimeInterval = time.Microsecond
 
 	go stats.Runtime(m)
 
-	time.Sleep(time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
 
-	m.AssertExpectations(t)
+	m.AssertCalled(t, "Gauge", "runtime.cpu.goroutines", mock.Anything, mock.Anything, mock.Anything)
 }
