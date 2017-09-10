80124bbc49eac9492858350b976a771ca37af541
Nicholas Wiersma <nick@wiersma.co.za>
2017-09-10 11:33:54 +0200

    Add l2met stats

---
+++ stats/l2met_test.go
@@ -0,0 +1,65 @@
+package stats_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/nrwiersma/pkg/stats"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestL2met_Inc(t *testing.T) {
+	l := &testLogger{}
+	s := stats.NewL2met(l, "test")
+
+	s.Inc("test", 2, 1.0, map[string]string{"test": "test"})
+
+	assert.Equal(t, "test=test count#test.test=2", l.msg)
+}
+
+func TestL2met_Dec(t *testing.T) {
+	l := &testLogger{}
+	s := stats.NewL2met(l, "test")
+
+	s.Dec("test", 2, 1.0, map[string]string{"test": "test"})
+
+	assert.Equal(t, "test=test count#test.test=-2", l.msg)
+}
+
+func TestL2met_Gauge(t *testing.T) {
+	l := &testLogger{}
+	s := stats.NewL2met(l, "test")
+
+	s.Gauge("test", 2.1, 1.0, map[string]string{"test": "test"})
+
+	assert.Equal(t, "test=test measure#test.test=2.1", l.msg)
+}
+
+func TestL2met_Timing(t *testing.T) {
+	l := &testLogger{}
+	s := stats.NewL2met(l, "test")
+
+	s.Timing("test", 2*time.Second, 1.0, map[string]string{"test": "test"})
+
+	assert.Equal(t, "test=test measure#test.test=2s", l.msg)
+}
+
+type testLogger struct {
+	msg string
+	ctx []interface{}
+}
+
+func (l *testLogger) Debug(msg string, ctx ...interface{}) {
+	l.msg = msg
+	l.ctx = ctx
+}
+
+func (l *testLogger) Info(msg string, ctx ...interface{}) {
+	l.msg = msg
+	l.ctx = ctx
+}
+
+func (l *testLogger) Error(msg string, ctx ...interface{}) {
+	l.msg = msg
+	l.ctx = ctx
+}
