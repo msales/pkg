b539745358ab71133b2929a8efc8c7237539e867
Nicholas Wiersma <nick@wiersma.co.za>
2018-10-26 11:34:30 +0200

    Fix test

--- health/server_test.go
+++ health/server_test.go
@@ -1,23 +1,20 @@
 package health_test
 
 import (
-	"net"
 	"testing"
-	"time"
 
 	"github.com/msales/pkg/health"
+	"github.com/msales/pkg/httpx"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStartServer(t *testing.T) {
 	r := &testReporter{}
 
-	go health.StartServer(":62874", r)
+	go health.StartServer("127.0.0.1:8080", r)
 	defer health.StopServer()
 
-	conn, err := net.DialTimeout("tcp", ":62874", time.Second)
+	resp, err := httpx.Get("http://127.0.0.1:8080/health")
 	assert.NoError(t, err)
-	if err == nil {
-		conn.Close()
-	}
+	assert.Equal(t, 200, resp.StatusCode)
 }
