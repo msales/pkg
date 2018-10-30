b7835a837b7b5138e86cc0953e14530cf2ff35ae
Nicholas Wiersma <nick@wiersma.co.za>
2018-10-30 14:05:02 +0200

    health lint and test (#29)

--- health/server.go
+++ health/server.go
@@ -1,24 +1,39 @@
 package health
 
 import (
+	"context"
 	"net/http"
 	"time"
 )
 
+// DefaultPattern is the default health http path.
 var DefaultPattern = "/health"
 
-func ListenAndServe(addr string, reporters ...Reporter) error {
-	return newServer(addr, reporters...).ListenAndServe()
+// DefaultHandler is the default health http handler.
+var DefaultHandler = NewHandler()
+
+var server = &http.Server{
+	ReadTimeout:  5 * time.Second,
+	WriteTimeout: 5 * time.Second,
 }
 
-func newServer(addr string, reporters ...Reporter) *http.Server {
+// StartServer starts the http health server on the given port
+// with the given reporters.
+func StartServer(addr string, reporters ...Reporter) error {
+	server.Addr = addr
+	server.Handler = newMux(reporters)
+
+	return server.ListenAndServe()
+}
+
+// StopServer stops the htt[ health server
+func StopServer() error {
+	return server.Shutdown(context.Background())
+}
+
+func newMux(reporters []Reporter) http.Handler {
 	mux := &http.ServeMux{}
 	mux.Handle(DefaultPattern, DefaultHandler.With(reporters...))
 
-	return &http.Server{
-		Addr:         addr,
-		Handler:      mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-	}
+	return mux
 }
