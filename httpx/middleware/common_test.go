a5ee6913a1d52c7f113cb576f5750504edc9a654
Michał Kurzeja <mike.kurzeja@gmail.com>
2019-01-16 13:08:12 +0100

    Added a quality-of-life common middleware to httpx (#40)

---
+++ httpx/middleware/common_test.go
@@ -0,0 +1,26 @@
+package middleware_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/msales/pkg/v3/httpx/middleware"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithCommon(t *testing.T) {
+	var called bool
+
+	h := middleware.WithCommon(context.Background(), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		called = true
+	}))
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	resp := httptest.NewRecorder()
+
+	h.ServeHTTP(resp, req)
+
+	assert.True(t, called)
+}
