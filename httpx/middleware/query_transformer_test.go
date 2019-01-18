76ecfbdf5277b77856ec4e1e5177c64890424a1a
Michał Kurzeja <mike.kurzeja@gmail.com>
2019-01-18 12:36:13 +0100

    Improved tests

--- httpx/middleware/query_transformer_test.go
+++ httpx/middleware/query_transformer_test.go
@@ -3,6 +3,7 @@ package middleware_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/msales/pkg/v3/httpx/middleware"
@@ -10,11 +11,13 @@ import (
 )
 
 func TestWithQueryTransformer(t *testing.T) {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "a=VA&b=VB&c=VC&d=vd", r.URL.RawQuery)
+	})
 	seenVals := make([]string, 0)
 	fn := func(s string) string {
 		seenVals = append(seenVals, s)
-		return s
+		return strings.ToUpper(s)
 	}
 
 	var tr middleware.QueryTransformer
@@ -33,45 +36,55 @@ func TestWithQueryTransformer(t *testing.T) {
 }
 
 func TestWithQueryTransformerFunc(t *testing.T) {
-	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 	url := "/?a=va&b=vb&c=vc&d=vd"
 
 	tests := []struct{
 		name string
 		keys []string
 		seenVals []string
+		wantQuery string
 	}{
 		{
 			name: "some keys defined",
 			keys: []string{"a", "b", "c"},
 			seenVals: []string{"va", "vb", "vc"},
+			wantQuery: "a=VA&b=VB&c=VC&d=vd",
 		},
 		{
 			name: "keys not in the query",
 			keys: []string{"x", "y", "z"},
 			seenVals: []string{},
+			wantQuery:"a=va&b=vb&c=vc&d=vd",
 		},
 		{
 			name: "no keys defined",
 			keys: []string{},
 			seenVals: []string{},
+			wantQuery:"a=va&b=vb&c=vc&d=vd",
 		},
 		{
 			name: "nil keys",
 			keys: nil,
 			seenVals: []string{},
+			wantQuery:"a=va&b=vb&c=vc&d=vd",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			var h http.Handler
+
+			h = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, tt.wantQuery, r.URL.RawQuery)
+
+			})
 			seenVals := make([]string, 0)
 			fn := func(s string) string {
 				seenVals = append(seenVals, s)
-				return s
+				return strings.ToUpper(s)
 			}
 
-			h := middleware.WithQueryTransformerFunc(dummyHandler, tt.keys, fn)
+			h = middleware.WithQueryTransformerFunc(h, tt.keys, fn)
 
 			req, _ := http.NewRequest("GET", url, nil)
 			resp := httptest.NewRecorder()
