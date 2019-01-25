7944028aad274b70027a0fea4082bbcb7c336642
Nicholas Wiersma <nick@wiersma.co.za>
2019-01-25 10:25:12 +0200

    Use mux internally

--- health/server.go
+++ health/server.go
@@ -1,9 +1,8 @@
 package health
 
 import (
-	"context"
-	"net/http"
-	"time"
+	"github.com/go-zoo/bone"
+	"github.com/msales/pkg/v3/httpx"
 )
 
 // DefaultPattern is the default health http path.
@@ -12,27 +11,9 @@ var DefaultPattern = "/health"
 // DefaultHandler is the default health http handler.
 var DefaultHandler = NewHandler()
 
-var server = &http.Server{
-	ReadTimeout:  5 * time.Second,
-	WriteTimeout: 5 * time.Second,
-}
-
-// StartServer starts the http health server on the given port
-// with the given reporters.
-func StartServer(addr string, reporters ...Reporter) error {
-	server.Addr = addr
-	server.Handler = newMux(reporters)
-
-	return server.ListenAndServe()
-}
-
-// StopServer stops the htt[ health server
-func StopServer() error {
-	return server.Shutdown(context.Background())
-}
-
-func newMux(reporters []Reporter) http.Handler {
-	mux := &http.ServeMux{}
+// NewMux creates a Mux for the health endpoint
+func NewMux(reporters ...Reporter) *bone.Mux {
+	mux := httpx.NewMux()
 	mux.Handle(DefaultPattern, DefaultHandler.With(reporters...))
 
 	return mux
