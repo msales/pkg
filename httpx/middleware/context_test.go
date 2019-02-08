185cf091dfb96f41a753be9f6c3d121a5ec784bc
Michał Kurzeja <mike.kurzeja@gmail.com>
2019-02-08 11:43:04 +0100

    httpx: WithContext middleware now merges the passed ctx to the request ctx instead of overwriting it

--- httpx/middleware/context_test.go
+++ httpx/middleware/context_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/msales/pkg/v3/httpx/middleware"
 	"github.com/stretchr/testify/assert"
@@ -14,11 +15,67 @@ func TestWithContext(t *testing.T) {
 	ctx := context.WithValue(context.Background(), "test", "test")
 
 	h := middleware.WithContext(ctx, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, ctx, r.Context())
+		assert.Equal(t, "test", r.Context().Value("test"))
 	}))
 
 	req, _ := http.NewRequest("GET", "/", nil)
 	resp := httptest.NewRecorder()
 
 	h.ServeHTTP(resp, req)
+}
+
+func TestWithContext_Value(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "test", "test")
+	reqCtx := context.WithValue(context.Background(), "req", "req")
+
+	h := middleware.WithContext(ctx, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "test", r.Context().Value("test"))
+		assert.Equal(t, "req", r.Context().Value("req"))
+	}))
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	resp := httptest.NewRecorder()
+
+	h.ServeHTTP(resp, req.WithContext(reqCtx))
+}
+
+func TestWithContext_WithDeadline(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "test", "test")
+
+	deadline, err := time.Parse(time.RFC822Z, "11 Nov 19 17:32 -0700")
+	if err != nil {
+		panic(err)
+	}
+
+	reqCtx, _ := context.WithDeadline(context.Background(), deadline)
+
+	h := middleware.WithContext(ctx, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "test", r.Context().Value("test"))
+
+		reqDeadline, ok := r.Context().Deadline()
+		assert.True(t, ok)
+		assert.Equal(t, deadline, reqDeadline)
+	}))
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	resp := httptest.NewRecorder()
+
+	h.ServeHTTP(resp, req.WithContext(reqCtx))
+}
+
+func TestWithContext_WithCancel(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "test", "test")
+	reqCtx, _ := context.WithCancel(context.Background())
+
+	h := middleware.WithContext(ctx, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "test", r.Context().Value("test"))
+
+		reqDone := r.Context().Done()
+		assert.NotNil(t, reqDone)
+	}))
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	resp := httptest.NewRecorder()
+
+	h.ServeHTTP(resp, req.WithContext(reqCtx))
 }
