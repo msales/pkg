b36f62b64a5159c18dacec6aa4b9c7fc705720d7
Michał Kurzeja <mike.kurzeja@gmail.com>
2019-04-18 14:33:34 +0200

    Added a stack trace to the http/grpc recovery log (#54)

--- grpcx/middleware/recovery.go
+++ grpcx/middleware/recovery.go
@@ -3,36 +3,75 @@ package middleware
 import (
 	"context"
 	"fmt"
+	"runtime/debug"
 
 	"github.com/msales/pkg/v3/log"
 	"google.golang.org/grpc"
 )
 
+// RecoveryFunc is used to configure the recovery interceptors.
+type RecoveryFunc func(*recoveryConfig)
+
+// WithoutStack disables the stack trace dump from the recovery log.
+func WithoutStack() RecoveryFunc {
+	return func(cfg *recoveryConfig) {
+		cfg.withStack = false
+	}
+}
+
+// recoveryConfig represents the configuration of the recovery interceptors.
+type recoveryConfig struct {
+	withStack bool
+}
+
+// newRecoveryConfig returns a new config object with sane defaults.
+func newRecoveryConfig(opts ...RecoveryFunc) *recoveryConfig {
+	cfg := &recoveryConfig{withStack: true}
+	cfg.applyOpts(opts)
+
+	return cfg
+}
+
+func (cfg *recoveryConfig) applyOpts(opts []RecoveryFunc) {
+	for _, fn := range opts {
+		fn(cfg)
+	}
+}
+
 // WithUnaryServerRecovery returns an interceptor that recovers from panics.
-func WithUnaryServerRecovery() grpc.UnaryServerInterceptor {
+func WithUnaryServerRecovery(opts ...RecoveryFunc) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-		defer recoveryFunc(ctx)
+		cfg := newRecoveryConfig(opts...)
+
+		defer recoveryFunc(ctx, cfg.withStack)
 
 		return handler(ctx, req)
 	}
 }
 
 // WithStreamServerRecovery returns an interceptor that recovers from panics.
-func WithStreamServerRecovery() grpc.StreamServerInterceptor {
+func WithStreamServerRecovery(opts ...RecoveryFunc) grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		defer recoveryFunc(ss.Context())
+		cfg := newRecoveryConfig(opts...)
+
+		defer recoveryFunc(ss.Context(), cfg.withStack)
 
 		return handler(srv, ss)
 	}
 }
 
-func recoveryFunc(ctx context.Context) {
+func recoveryFunc(ctx context.Context, withStack bool) {
 	if v := recover(); v != nil {
 		err := fmt.Errorf("%v", v)
 		if v, ok := v.(error); ok {
 			err = v
 		}
 
-		log.Error(ctx, err.Error())
+		var logCtx []interface{}
+		if withStack {
+			logCtx = append(logCtx, "stack", string(debug.Stack()))
+		}
+
+		log.Error(ctx, err.Error(), logCtx...)
 	}
 }
--- httpx/middleware/recovery.go
+++ httpx/middleware/recovery.go
@@ -3,24 +3,44 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/msales/pkg/v3/log"
 )
 
-// Recovery is a middleware that will recover from panics and logs the error.
-type Recovery struct {
-	handler http.Handler
-}
 
-// WithRecovery recovers from panics and log the error.
-func WithRecovery(h http.Handler) http.Handler {
-	return &Recovery{
-		handler: h,
+// RecoveryFunc is used to configure the recovery handler.
+type RecoveryFunc func(*Recovery)
+
+// WithoutStack disables the stack trace dump from the recovery log.
+func WithoutStack() RecoveryFunc {
+	return func(r *Recovery) {
+		r.withStack = false
 	}
 }
 
+// Recovery is a middleware that will recover from panics and logs the error.
+type Recovery struct {
+	handler   http.Handler
+	withStack bool
+}
+
+// WithRecovery recovers from panics and log the error.
+func WithRecovery(h http.Handler, opts ...RecoveryFunc) http.Handler {
+	r := &Recovery{
+		handler:   h,
+		withStack: true,
+	}
+
+	for _, fn := range opts {
+		fn(r)
+	}
+
+	return r
+}
+
 // ServeHTTP serves the request.
-func (m Recovery) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (m *Recovery) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if v := recover(); v != nil {
 			err := fmt.Errorf("%v", v)
@@ -28,7 +48,12 @@ func (m Recovery) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				err = v
 			}
 
-			log.Error(r.Context(), err.Error())
+			var logCtx []interface{}
+			if m.withStack {
+				logCtx = append(logCtx, "stack", string(debug.Stack()))
+			}
+
+			log.Error(r.Context(), err.Error(), logCtx...)
 			w.WriteHeader(500)
 		}
 	}()
