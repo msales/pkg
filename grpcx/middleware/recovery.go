8c1991d9eedbc6b0e5a88ebce4d4a13eb03efb54
Michał Kurzeja <mike.kurzeja@gmail.com>
2019-04-18 14:18:05 +0200

    added recovery config object

--- grpcx/middleware/recovery.go
+++ grpcx/middleware/recovery.go
@@ -9,29 +9,34 @@ import (
 	"google.golang.org/grpc"
 )
 
+// recoveryConfig represents the configuration of the recovery interceptors.
+type recoveryConfig struct {
+	withStack bool
+}
+
 // WithUnaryServerRecovery returns an interceptor that recovers from panics.
-func WithUnaryServerRecovery(stackOpts ...func() bool) grpc.UnaryServerInterceptor {
+func WithUnaryServerRecovery(opts ...func(*recoveryConfig)) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-		withStack := true
-		for _, fn := range stackOpts {
-			withStack = fn()
+		cfg := &recoveryConfig{withStack: true}
+		for _, fn := range opts {
+			fn(cfg)
 		}
 
-		defer recoveryFunc(ctx, withStack)
+		defer recoveryFunc(ctx, cfg.withStack)
 
 		return handler(ctx, req)
 	}
 }
 
 // WithStreamServerRecovery returns an interceptor that recovers from panics.
-func WithStreamServerRecovery(stackOpts ...func() bool) grpc.StreamServerInterceptor {
+func WithStreamServerRecovery(opts ...func(*recoveryConfig)) grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		withStack := true
-		for _, fn := range stackOpts {
-			withStack = fn()
+		cfg := &recoveryConfig{withStack: true}
+		for _, fn := range opts {
+			fn(cfg)
 		}
 
-		defer recoveryFunc(ss.Context(), withStack)
+		defer recoveryFunc(ss.Context(), cfg.withStack)
 
 		return handler(srv, ss)
 	}
@@ -54,8 +59,8 @@ func recoveryFunc(ctx context.Context, withStack bool) {
 }
 
 // WithoutStack disables the stack trace dump from the recovery log.
-func WithoutStack() func() bool {
-	return func() bool {
-		return false
+func WithoutStack() func(*recoveryConfig) {
+	return func(cfg *recoveryConfig) {
+		cfg.withStack = false
 	}
 }
