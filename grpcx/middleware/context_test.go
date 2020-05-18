57ffb22fa42a3f0352e269c46bab7a5f39d610f2
Hugon Sknadaj <hugon.sknadaj@msales.com>
2020-05-18 10:49:17 +0200

    Added client middlewares to grpcx packages + common options for client.

--- grpcx/middleware/context_test.go
+++ grpcx/middleware/context_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/msales/pkg/v3/grpcx/middleware"
 	"github.com/msales/pkg/v3/log"
@@ -68,6 +69,51 @@ func TestWithStreamServerStats(t *testing.T) {
 
 	err := interceptor(nil, stream, nil, func(srv interface{}, stream grpc.ServerStream) error {
 		s, ok := stats.FromContext(stream.Context())
+
+		assert.Equal(t, s, stats.Null)
+		assert.True(t, ok)
+
+		return testErr
+	})
+
+	assert.Equal(t, testErr, err)
+}
+
+func TestWithUnaryClientContextTimeout(t *testing.T) {
+	ctx := context.Background()
+
+	interceptor := middleware.WithUnaryClientContextTimeout(1 * time.Hour)
+	err := interceptor(ctx, "method", nil, nil, nil, func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+		_, ok := ctx.Deadline()
+
+		assert.True(t, ok)
+
+		return testErr
+	})
+
+	assert.Equal(t, testErr, err)
+}
+
+func TestWithUnaryClientLogger(t *testing.T) {
+	interceptor := middleware.WithUnaryClientLogger(log.Null)
+
+	err := interceptor(context.Background(), "method", nil, nil, nil, func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+		l, ok := log.FromContext(ctx)
+
+		assert.Equal(t, l, log.Null)
+		assert.True(t, ok)
+
+		return testErr
+	})
+
+	assert.Equal(t, testErr, err)
+}
+
+func TestWithUnaryClientStats(t *testing.T) {
+	interceptor := middleware.WithUnaryClientStats(stats.Null)
+
+	err := interceptor(context.Background(), "method", nil, nil, nil, func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+		s, ok := stats.FromContext(ctx)
 
 		assert.Equal(t, s, stats.Null)
 		assert.True(t, ok)
