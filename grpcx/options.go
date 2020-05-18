680334a5d11c5fee9e51d284edda75f25e33a4e6
Hugon Sknadaj <hugon.sknadaj@msales.com>
2020-05-18 10:45:32 +0200

    Added client middlewares to grpcx packages + common options for client

--- grpcx/options.go
+++ grpcx/options.go
@@ -2,12 +2,33 @@ package grpcx
 
 import (
 	"context"
+	"time"
 
 	"github.com/msales/pkg/v4/grpcx/middleware"
 	"github.com/msales/pkg/v4/log"
 	"github.com/msales/pkg/v4/stats"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
 )
+
+// UnaryClientCommonOpts returns commonly options for an unary client.
+func UnaryClientCommonOpts(ctx context.Context, timeout time.Duration, additional ...grpc.UnaryClientInterceptor) []grpc.DialOption {
+	l, s := getLoggerAndStats(ctx)
+
+	interceptors := []grpc.UnaryClientInterceptor{
+		middleware.WithUnaryClientLogger(l),
+		middleware.WithUnaryClientStats(s),
+		middleware.WithUnaryClientContextTimeout(timeout),
+	}
+
+	interceptors = append(interceptors, additional...)
+
+	return []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBalancerName(roundrobin.Name),
+		middleware.WithUnaryClientInterceptors(interceptors...),
+	}
+}
 
 // UnaryServerCommonOpts returns commonly options for an unary server.
 func UnaryServerCommonOpts(ctx context.Context, statsTagsFns ...TagsFunc) []grpc.ServerOption {
