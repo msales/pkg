1aaa9d65a76cf6839cd3df33fe347c35ff3f46ef
Hugon Sknadaj <hugon94@gmail.com>
2020-06-19 13:48:04 +0200

    Add name to breaker to differentiate in stats + added client panic recovery. (#165)

--- grpcx/middleware/breaker.go
+++ grpcx/middleware/breaker.go
@@ -12,21 +12,22 @@ import (
 const (
 	breakerErrorKey = "breaker.error"
 	stateTag        = "state"
+	nameTag         = "name"
 )
 
 // WithClientBreaker adds breaker to client request.
-func WithClientBreaker(br *breaker.Breaker) grpc.UnaryClientInterceptor {
+func WithClientBreaker(br *breaker.Breaker, name string) grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		err := br.Run(func() error {
 			return invoker(ctx, method, req, reply, cc, opts...)
 		})
 
 		if errors.Is(err, breaker.ErrOpenState) {
-			_ = stats.Inc(ctx, breakerErrorKey, 1, 1.0, stateTag, "open")
+			_ = stats.Inc(ctx, breakerErrorKey, 1, 1.0, stateTag, "open", nameTag, name)
 		}
 
 		if errors.Is(err, breaker.ErrTooManyRequests) {
-			_ = stats.Inc(ctx, breakerErrorKey, 1, 1.0, stateTag, "too_many_requests")
+			_ = stats.Inc(ctx, breakerErrorKey, 1, 1.0, stateTag, "too_many_requests", nameTag, name)
 		}
 
 		return err
--- grpcx/middleware/breaker_test.go
+++ grpcx/middleware/breaker_test.go
@@ -7,7 +7,9 @@ import (
 	"time"
 
 	"github.com/msales/pkg/v3/breaker"
+	"github.com/msales/pkg/v3/stats"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc"
 
 	. "github.com/msales/pkg/v3/grpcx/middleware"
@@ -16,24 +18,30 @@ import (
 var breakerErr = errors.New("breaker: circuit breaker is open")
 
 func TestWithBreaker(t *testing.T) {
+	s := new(mockStats)
+	s.AssertNotCalled(t, "Inc")
 	ctx := context.Background()
+	ctx = stats.WithStats(ctx, s)
 
 	br := breaker.NewBreaker(
 		breaker.RateFuse(1),
 		breaker.WithSleep(1*time.Second),
 		breaker.WithTestRequests(1),
 	)
-
-	interceptor := WithClientBreaker(br)
+	interceptor := WithClientBreaker(br, "test")
 	err := interceptor(ctx, "method", nil, nil, nil, func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
 		return nil
 	})
 
 	assert.Nil(t, err)
+	s.AssertExpectations(t)
 }
 
 func TestWithBreaker_Errored(t *testing.T) {
+	s := new(mockStats)
+	s.On("Inc", "breaker.error", int64(1), float32(1.0), []interface{}{"state", "open", "name", "test"}).Return(nil).Once()
 	ctx := context.Background()
+	ctx = stats.WithStats(ctx, s)
 
 	br := breaker.NewBreaker(
 		breaker.RateFuse(10),
@@ -41,7 +49,7 @@ func TestWithBreaker_Errored(t *testing.T) {
 		breaker.WithTestRequests(1),
 	)
 
-	interceptor := WithClientBreaker(br)
+	interceptor := WithClientBreaker(br, "test")
 	err := interceptor(ctx, "method", nil, nil, nil, func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
 		return testErr
 	})
@@ -53,4 +61,34 @@ func TestWithBreaker_Errored(t *testing.T) {
 	})
 
 	assert.Equal(t, breakerErr, err)
+	s.AssertExpectations(t)
+}
+
+type mockStats struct {
+	mock.Mock
+}
+
+func (m *mockStats) Inc(name string, value int64, rate float32, tags ...interface{}) error {
+	args := m.Called(name, value, rate, tags)
+	return args.Error(0)
+}
+
+func (m *mockStats) Dec(name string, value int64, rate float32, tags ...interface{}) error {
+	args := m.Called(name, value, rate, tags)
+	return args.Error(0)
+}
+
+func (m *mockStats) Gauge(name string, value float64, rate float32, tags ...interface{}) error {
+	args := m.Called(name, value, rate, tags)
+	return args.Error(0)
+}
+
+func (m *mockStats) Timing(name string, value time.Duration, rate float32, tags ...interface{}) error {
+	args := m.Called(name, value, rate, tags)
+	return args.Error(0)
+}
+
+func (m *mockStats) Close() error {
+	args := m.Called()
+	return args.Error(0)
 }
