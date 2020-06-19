e230bb72bcb45204bbf339aa6ada971e61195411
Hugon Sknadaj <hugon.sknadaj@msales.com>
2020-06-19 09:40:50 +0200

    Add name to breaker to differentiate in stats + added client panic recovery.

--- grpcx/options.go
+++ grpcx/options.go
@@ -11,6 +11,26 @@ import (
 	"google.golang.org/grpc/balancer/roundrobin"
 )
 
+// UnaryClientCommonOpts returns commonly options for an unary client.
+func UnaryClientCommonOpts(ctx context.Context, timeout time.Duration, additional ...grpc.UnaryClientInterceptor) []grpc.DialOption {
+	l, s := getLoggerAndStats(ctx)
+
+	interceptors := []grpc.UnaryClientInterceptor{
+		middleware.WithUnaryClientLogger(l),
+		middleware.WithUnaryClientStats(s),
+		middleware.WithUnaryClientRecovery(),
+		middleware.WithUnaryClientContextTimeout(timeout),
+	}
+
+	interceptors = append(interceptors, additional...)
+
+	return []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBalancerName(roundrobin.Name),
+		middleware.WithUnaryClientInterceptors(interceptors...),
+	}
+}
+
 // UnaryServerCommonOpts returns commonly options for an unary server.
 func UnaryServerCommonOpts(ctx context.Context, statsTagsFns ...TagsFunc) []grpc.ServerOption {
 	l, s := getLoggerAndStats(ctx)
@@ -40,25 +60,6 @@ func StreamServerCommonOpts(ctx context.Context, statsTagsFns ...TagsFunc) []grp
 		grpc.StatsHandler(
 			WithRPCStats(s, statsTagsFns...),
 		),
-	}
-}
-
-// UnaryClientCommonOpts returns commonly options for an unary client.
-func UnaryClientCommonOpts(ctx context.Context, timeout time.Duration, additional ...grpc.UnaryClientInterceptor) []grpc.DialOption {
-	l, s := getLoggerAndStats(ctx)
-
-	interceptors := []grpc.UnaryClientInterceptor{
-		middleware.WithUnaryClientLogger(l),
-		middleware.WithUnaryClientStats(s),
-		middleware.WithUnaryClientContextTimeout(timeout),
-	}
-
-	interceptors = append(interceptors, additional...)
-
-	return []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithBalancerName(roundrobin.Name),
-		middleware.WithUnaryClientInterceptors(interceptors...),
 	}
 }
 
